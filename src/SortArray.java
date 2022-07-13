89fd0977135fb4025bad2f787eeefb32b7452dfa
priyanka-geesa <97487716+priyanka-geesa@users.noreply.github.com>
2022-07-13 13:57:39 -0500

    added a new variable

--- src/SortArray.java
+++ src/SortArray.java
@@ -4,6 +4,7 @@ public class SortArray
 {
     public static void main(String[] args)
     {
+        int temp = "";
       int [] arr1 = { 1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365, 1456, 2165, 1457, 2456};
       System.out.println("Original Array : " + Arrays.toString(arr1));
       Arrays.sort(arr1);
